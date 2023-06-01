297fbce9f940c26830cbabc4c85391c2c69b1a75
zk <1099571219@qq.com>
2023-06-01 18:06:42 +0800

    build(custom): 添加 cli commit 检测 、格式化

--- src/create.js
+++ src/create.js
@@ -1,46 +1,51 @@
 #! /usr/bin/env node
-const symbol = require("log-symbols");
-const program = require("commander");
-const inquirer = require("inquirer");
-const package = require("../package.json");
-const templates = require("./templates");
-const downloadGitRepo = require("download-git-repo");
-const path = require("path");
-const { exec } = require("child_process");
-const { notExistFolder, cloneLibrary, selectTemplate, inputFileName } = require("./util");
-const chalk = require("chalk");
+const symbol = require('log-symbols')
+const program = require('commander')
+const inquirer = require('inquirer')
+const package = require('../package.json')
+const templates = require('./templates')
+const downloadGitRepo = require('download-git-repo')
+const path = require('path')
+const { exec } = require('child_process')
+const {
+  notExistFolder,
+  cloneLibrary,
+  selectTemplate,
+  inputFileName,
+} = require('./util')
+const chalk = require('chalk')
 const fs = require('fs-extra')
 
 program
-  .command("create [projectName]")
-  .description("创建模板")
-  .option("-t,--template <template>", "模板名称")
+  .command('create [projectName]')
+  .description('创建模板')
+  .option('-t,--template <template>', '模板名称')
   .action(async (projectName, options) => {
     let project = templates.find(
-      (template) => template.name === options.template
-    );
-    let projectTemplate = project ? project.value : undefined;
+      (template) => template.name === options.template,
+    )
+    let projectTemplate = project ? project.value : undefined
 
     if (!projectName) {
       projectName = await inputFileName()
     }
-    console.log("项目名称:", projectName);
-    const directory = process.cwd();
+    console.log('项目名称:', projectName)
+    const directory = process.cwd()
 
-    await notExistFolder(directory, projectName);
+    await notExistFolder(directory, projectName)
 
     if (!projectTemplate) {
-      projectTemplate = await selectTemplate();
+      projectTemplate = await selectTemplate()
     }
 
     // 目标文件夹 = 用户命令行所在目录 + 项目名称
-    await cloneLibrary(projectTemplate, directory, projectName);
-    console.log("同步");
-    console.log("异步", projectName);
-  });
+    await cloneLibrary(projectTemplate, directory, projectName)
+    console.log('同步')
+    console.log('异步', projectName)
+  })
 
-program.on("--help", () => {});
-program.version(`v${package.version}`);
+program.on('--help', () => {})
+program.version(`v${package.version}`)
 
 //解析用户执行命令行传入的参数
-program.parse(process.argv);
+program.parse(process.argv)
--- src/util.js
+++ src/util.js
@@ -1,94 +1,93 @@
-const symbol = require("log-symbols");
-const chalk = require("chalk");
-const fs = require("fs-extra");
-const { exec, spawn } = require("child_process");
-const ora = require("ora");
-const path = require("path");
-const inquirer = require("inquirer");
-const templates = require("./templates");
+const symbol = require('log-symbols')
+const chalk = require('chalk')
+const fs = require('fs-extra')
+const { exec, spawn } = require('child_process')
+const ora = require('ora')
+const path = require('path')
+const inquirer = require('inquirer')
+const templates = require('./templates')
 
 const templateRoot =
-  "http://10.10.16.211:8181/digital-twin-project-team-of-xinda-group/front-end-templates.git";
+  'http://10.10.16.211:8181/digital-twin-project-team-of-xinda-group/front-end-templates.git'
 
 const templateFileName = templateRoot.substring(
-  templateRoot.lastIndexOf("/") + 1,
-  templateRoot.lastIndexOf(".")
-);
+  templateRoot.lastIndexOf('/') + 1,
+  templateRoot.lastIndexOf('.'),
+)
 
 const inputFileName = async () => {
   const { name } = await inquirer.prompt({
-    type: "input",
-    name: "name",
-    message: "请输入项目名称:",
-  });
-  if (!name) console.log(symbol.error, chalk.red("项目名称不能为空"));
-  return name;
-};
+    type: 'input',
+    name: 'name',
+    message: '请输入项目名称:',
+  })
+  if (!name) console.log(symbol.error, chalk.red('项目名称不能为空'))
+  return name
+}
 
 const selectTemplate = async () => {
   // 新增选择模版代码;
   const { template } = await inquirer.prompt({
-    type: "list",
-    name: "template",
-    message: "请选择模版：",
+    type: 'list',
+    name: 'template',
+    message: '请选择模版：',
     choices: templates, // 模版列表
-  });
-  return template;
-};
+  })
+  return template
+}
 
 const cover = async (directory, fileName) => {
   if (fs.existsSync(`${directory}\\${fileName}`)) {
     const { force } = await inquirer.prompt({
-      type: "confirm",
-      name: "force",
+      type: 'confirm',
+      name: 'force',
       message: `${fileName} 目录已存在，是否覆盖?`,
-    });
-    force ? fs.removeSync(`${directory}\\${fileName}`) : process.exit(1);
+    })
+    force ? fs.removeSync(`${directory}\\${fileName}`) : process.exit(1)
   }
-};
+}
 
 // 文件是否存在
 let notExistFolder = async (directory, fileName) => {
-  await cover(directory, templateFileName);
-  await cover(directory, fileName);
-  return;
-};
+  await cover(directory, templateFileName)
+  await cover(directory, fileName)
+  return
+}
 
 async function cloneLibrary(branch, directory, fileName) {
-  process.chdir(directory);
+  process.chdir(directory)
   // console.log(`${directory}\\${fileName}`);
   return new Promise((resolve) => {
-    const loading = ora("正在下载模板...");
-    loading.start();
+    const loading = ora('正在下载模板...')
+    loading.start()
     exec(`git clone -b ${branch} ${templateRoot}`, (error, stdout, stderr) => {
       if (error) {
-        console.log(`error: ${error}`);
-        loading.fail(`创建模板失败: ${error}`);
+        console.log(`error: ${error}`)
+        loading.fail(`创建模板失败: ${error}`)
       }
-      stdout && console.log(`stdout: ${stdout}`);
-      stderr && console.log(`stderr: ${stderr}`);
-      loading.succeed("创建模板成功!");
+      stdout && console.log(`stdout: ${stdout}`)
+      stderr && console.log(`stderr: ${stderr}`)
+      loading.succeed('创建模板成功!')
 
-      const loadPackage = ora("下载依赖中....");
-      loadPackage.start();
+      const loadPackage = ora('下载依赖中....')
+      loadPackage.start()
       fs.rename(
         `${directory}\\${templateFileName}`,
         `${directory}\\${fileName}`,
         (error) => {
-          error && console.log(symbol.error, chalk.red(error));
+          error && console.log(symbol.error, chalk.red(error))
           exec(`cd ${fileName} && npm install`, (error, stdout, stderr) => {
             error &&
-              (console.log(`error: ${error}`),
-              loadPackage.fail("依赖下载失败"));
-            stdout && console.log(`stdout: ${stdout}`);
-            stderr && console.log(`stderr: ${stderr}`);
-            loadPackage.succeed("依赖安装完成！");
+              (console.log(`error: ${error}`), loadPackage.fail('依赖下载失败'))
+            stdout && console.log(`stdout: ${stdout}`)
+            stderr && console.log(`stderr: ${stderr}`)
+            loadPackage.succeed('依赖安装完成！')
             resolve()
-          });
-        }
-      );
-    });
-  });
+          })
+        },
+      )
+    })
+  })
 }
 
 module.exports = {
@@ -96,4 +95,4 @@ module.exports = {
   cloneLibrary,
   selectTemplate,
   inputFileName,
-};
+}
